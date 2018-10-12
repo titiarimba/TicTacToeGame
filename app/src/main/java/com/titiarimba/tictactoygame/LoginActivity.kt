45fad2c48fa69f30259679d362948f6bfb1aceaf
titirimba <titiarimba597@gmail.com>
2018-10-13 00:11:58 +0700

    login

---
+++ app/src/main/java/com/titiarimba/tictactoygame/LoginActivity.kt
@@ -0,0 +1,17 @@
+/*
+ * Created by Titi Arimba on 10/13/18 12:10 AM
+ * Last Modified on 10/13/18 12:10 AM
+ */
+
+package com.titiarimba.tictactoygame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
